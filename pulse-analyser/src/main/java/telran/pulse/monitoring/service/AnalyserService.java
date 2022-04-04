723da9609b4b84fb2e6528805adec8beb84c6159
Oleg <snaik.men@gmail.com>
2022-04-04 15:32:48 +0300

    init commit

---
+++ pulse-analyser/src/main/java/telran/pulse/monitoring/service/AnalyserService.java
@@ -0,0 +1,28 @@
+package telran.pulse.monitoring.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import telran.pulse.monitoring.dto.Sensor;
+
+import java.util.function.Consumer;
+
+@Service
+@AllArgsConstructor
+public class AnalyserService {
+
+    private StreamBridge streamBridge;
+    @Bean
+    Consumer<Sensor> pulseConsumer() {
+        return this::pulseProcessing;
+    }
+
+    private void pulseProcessing(Sensor sensor) {
+        System.out.printf("sequence number %d, sensor id %d, waiting time %d\n",
+                sensor.seqNum,
+                sensor.id,
+                System.currentTimeMillis() - sensor.timestamp);
+    }
+
+}
